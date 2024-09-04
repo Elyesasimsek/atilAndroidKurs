cb5d2686099e2496f11719ae8538502a86432f04
Elyesasimsek <rabiyelyesa1999@gmail.com>
2024-09-04 20:52:14 +0300

    atilAndroidKurs

--- 004)SimpleCalculator/app/src/main/java/com/example/a004simplecalculator/MainActivity.java
+++ 004)SimpleCalculator/app/src/main/java/com/example/a004simplecalculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.a004simplecalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextNumber1, editTextNumber2;
+    private TextView textViewResultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,52 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        editTextNumber1 = findViewById(R.id.editTextNumber1);
+        editTextNumber2 = findViewById(R.id.editTextNumber2);
+        textViewResultText = findViewById(R.id.textViewResultText);
+    }
+
+    public void sum(View view){
+        if (editTextNumber1.getText().toString().matches("") || editTextNumber2.getText().toString().matches("")){
+            textViewResultText.setText("Enter Number");
+        }else{
+            int number1 =  Integer.parseInt(editTextNumber1.getText().toString());
+            int number2 =  Integer.parseInt(editTextNumber2.getText().toString());
+            int result = number1 + number2;
+            textViewResultText.setText("Result: " + result);
+        }
+    }
+
+    public void deduct(View view){
+        if (editTextNumber1.getText().toString().matches("") || editTextNumber2.getText().toString().matches("")){
+            textViewResultText.setText("Enter Number");
+        }else{
+            int number1 =  Integer.parseInt(editTextNumber1.getText().toString());
+            int number2 =  Integer.parseInt(editTextNumber2.getText().toString());
+            int result = number1 - number2;
+            textViewResultText.setText("Result: " + result);
+        }
+    }
+
+    public void multiply(View view){
+        if (editTextNumber1.getText().toString().matches("") || editTextNumber2.getText().toString().matches("")){
+            textViewResultText.setText("Enter Number");
+        }else{
+            int number1 =  Integer.parseInt(editTextNumber1.getText().toString());
+            int number2 =  Integer.parseInt(editTextNumber2.getText().toString());
+            int result = number1 * number2;
+            textViewResultText.setText("Result: " + result);
+        }
+    }
+
+    public void divide(View view){
+        if (editTextNumber1.getText().toString().matches("") || editTextNumber2.getText().toString().matches("")){
+            textViewResultText.setText("Enter Number");
+        }else{
+            int number1 =  Integer.parseInt(editTextNumber1.getText().toString());
+            int number2 =  Integer.parseInt(editTextNumber2.getText().toString());
+            int result = number1 / number2;
+            textViewResultText.setText("Result: " + result);
+        }
     }
 }
