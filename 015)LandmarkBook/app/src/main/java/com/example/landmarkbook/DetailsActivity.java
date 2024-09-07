168f95dc7026acdec128778be9898ac0de977433
Elyesasimsek <rabiyelyesa1999@gmail.com>
2024-09-07 19:57:26 +0300

    atilAndroidKurs

--- 015)LandmarkBook/app/src/main/java/com/example/landmarkbook/DetailsActivity.java
+++ 015)LandmarkBook/app/src/main/java/com/example/landmarkbook/DetailsActivity.java
@@ -1,6 +1,8 @@
 package com.example.landmarkbook;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.landmarkbook.databinding.ActivityDetailsBinding;
+
 public class DetailsActivity extends AppCompatActivity {
+
+    private ActivityDetailsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_details);
+        binding = ActivityDetailsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Intent intent = getIntent();
+        Landmark selectedLandmark = (Landmark) intent.getSerializableExtra("landmark");
+        binding.textViewNameText.setText(selectedLandmark.name);
+        binding.textViewCountryTexxt.setText(selectedLandmark.country);
+        binding.imageView.setImageResource(selectedLandmark.image);
     }
 }
--- 015)LandmarkBook/app/src/main/java/com/example/landmarkbook/MainActivity.java
+++ 015)LandmarkBook/app/src/main/java/com/example/landmarkbook/MainActivity.java
@@ -1,24 +1,52 @@
 package com.example.landmarkbook;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.landmarkbook.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Landmark> landmarkArrayList;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        landmarkArrayList = new ArrayList<>();
+
+        binding.rv.setHasFixedSize(true);
+        binding.rv.setLayoutManager(new LinearLayoutManager(this));
+
+       Landmark pisa = new Landmark("Pisa", "Italy", R.drawable.pisa);
+       Landmark eifeel = new Landmark("Eifeel", "France", R.drawable.eyfel);
+       Landmark colesseum = new Landmark("Colesseum", "Italy", R.drawable.colosseum);
+       Landmark londonBridge = new Landmark("London Bridge", "UK", R.drawable.londonbridge);
+
+       landmarkArrayList.add(pisa);
+       landmarkArrayList.add(eifeel);
+       landmarkArrayList.add(colesseum);
+       landmarkArrayList.add(londonBridge);
+
+       LandmarkAdapter landmarkAdapter = new LandmarkAdapter(landmarkArrayList);
+       binding.rv.setAdapter(landmarkAdapter);
     }
 }
