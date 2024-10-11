9d9e25ba6f753046199bd373e518aadadf0d6093
Elyesasimsek <rabiyelyesa1999@gmail.com>
2024-10-11 15:34:45 +0300

    atilAndroidKurs

--- 065)StateManagementCompose/app/src/main/java/com/elyesasimsek/statemanagementcompose/MainActivity.kt
+++ 065)StateManagementCompose/app/src/main/java/com/elyesasimsek/statemanagementcompose/MainActivity.kt
@@ -9,18 +9,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.elyesasimsek.statemanagementcompose.ui.theme.StateManagementComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,31 +34,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen() {
-    Surface(
-        color = Color.LightGray
-    ) {
-        Column(modifier = Modifier.fillMaxSize(),
+fun MainScreen(){
+
+    var myString = remember {
+        mutableStateOf("")
+    }
+
+    Surface (
+        modifier = Modifier.fillMaxSize()
+    ){
+        Column (
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        )  {
-            Text(
-                text = "Hello!",
-                fontSize = 26.sp
-            )
-
-            Spacer(modifier = Modifier.padding(7.dp))
-
-            Button(onClick = {println("clicked")}) {
-                Text(text = "Button")
-                Text(text = "Test")
+        ){
+            SpecialText(string = "Test")
+            Spacer(modifier = Modifier.padding(5.dp))
+            SpecialText(string = "Android")
+            Spacer(modifier = Modifier.padding(5.dp))
+            SpecialTextField(myString.value){
+                myString.value = it
             }
         }
     }
 }
 
+@Composable
+fun SpecialText(string: String){
+    Text(
+        text = string,
+        fontSize = 20.sp,
+        fontStyle = FontStyle.Italic,
+        fontFamily = FontFamily.SansSerif
+    )
+}
+
+@Composable
+fun SpecialTextField(string: String, function: (String) -> Unit){
+    TextField(
+        value = string,
+        onValueChange = function
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-        MainScreen()
+    MainScreen()
 }
